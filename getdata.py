9ad54425398cc5212f97efaaabd41475c9a420b3
thanpisitsu <Thanpisitsuriyaroj@gmail.com>
2023-09-27 21:34:03 +0700

    final

--- getdata.py
+++ getdata.py
@@ -11,6 +11,11 @@ def digitleft(str) :
         str = str[:-1]
     return str
 
+def remove(str) :
+    while str[-1]!='"':
+        str = str[:-1]
+    return str[:-3]
+
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
 }
@@ -22,26 +27,23 @@ if os.path.exists(creds_file):
 service = build('sheets', 'v4', credentials=creds)
 spreadsheet_id = '1ixexWYv2zIZn0Fc0RfIXhY7rGn5UsolaFWCKRA8isJc'
 
-f=254
+f=435
 datasheet = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range=f'boardgamerank!A{f}:B500').execute()['values']
 list=[]
-
 
 for i in datasheet :
     url = "https://boardgamegeek.com/boardgame/"+ '/'.join(i)
     response = requests.get(url, headers=headers)
     soup = BeautifulSoup(response.text, "html.parser")
-    print(str(soup))
     
     keyweight = '"avgweight":"'
     keyminplayer = '"minplayers":"'
     keymaxplayer = '"maxplayers":"'
     keyvote = '"numweights":"'
     keyplaytime = '"maxplaytime":"'
+    keytype = '"subdomain":"'
     datastat = str(soup)
-    range = datastat.find('"rank":"')
-    datastat = datastat[range:range+2000]
-
+    print(i)
     error = soup.find('div',class_='messagebox error')
     if not error :
         game_title = soup.find("meta", property="og:title")["content"]
@@ -50,14 +52,17 @@ for i in datasheet :
         keyminplayer_index = datastat.find(keyminplayer)
         keymaxplayer_index = datastat.find(keymaxplayer)
         keyplaytime_index = datastat.find(keyplaytime)
+        keytype_index = datastat.find(keytype)
         game_keyvote = digitleft(datastat[keyvote_index + len(keyvote):keyvote_index + len(keyvote) + 10])
         game_weight = digitleft(datastat[keyweight_index + len(keyweight):keyweight_index + len(keyweight) + 10])
         game_minplayer = digitleft(datastat[keyminplayer_index + len(keyminplayer):keyminplayer_index + len(keyminplayer) + 10])
         game_maxplayer = digitleft(datastat[keymaxplayer_index + len(keymaxplayer):keymaxplayer_index + len(keymaxplayer) + 10])
         game_playtime = digitleft(datastat[keyplaytime_index + len(keyplaytime):keyplaytime_index + len(keyplaytime) + 10])
+        game_type = remove(datastat[keytype_index + len(keytype):keytype_index + len(keytype) + 20])
 
-        data = [[game_title,float(game_weight)*100,int(game_minplayer),int(game_maxplayer),int(game_playtime)]]
+        data = [[game_title,game_type,float(game_weight)*100,int(game_minplayer),int(game_maxplayer),int(game_playtime)]]
+        print(data)
 
-        request = service.spreadsheets().values().update(spreadsheetId=spreadsheet_id, range=f'boardgame!A{f+1}:E{f+1}', valueInputOption="RAW", body={"values": data})
+        request = service.spreadsheets().values().update(spreadsheetId=spreadsheet_id, range=f'boardgame!A{f+1}:F{f+1}', valueInputOption="RAW", body={"values": data})
         response = request.execute()
         f+=1
